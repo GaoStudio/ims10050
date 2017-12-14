717a6ab5761ff0c07aaeddded709073208bac018
gaowei <gaoweivf@aliyun.com>
2017-12-14 19:22:27 +0800

    change

---
+++ src/routes/Books/BookBorrow.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class BookBorrow extends Component {
+  render() {
+    return <div>BookBorrow</div>;
+  }
+}
--- src/routes/Books/components/BookCase.js
+++ src/routes/Books/components/BookCase.js
@@ -1,62 +1,71 @@
-import React, { PureComponent } from 'react';
-import { Card, Modal, Button, Icon, List } from 'antd';
-import styles from '../BookStore.less';
-import BookCaseAdd from './BookCaseAdd';
+import React, { PureComponent } from "react";
+import { Card, Modal, Button, Icon, List } from "antd";
+import styles from "../BookStore.less";
+import BookCaseAdd from "./BookCaseAdd";
 
 export default class BookCase extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       visible: false,
-      caselists: [{
-        id: 1,
-        avatar: 'hah',
-        title: 'fdsf',
-        description: 'fds',
-        layers: [
-          {
-            layer: 1,
-            number: 22,
-          },
-          {
-            layer: 2,
-            number: 25,
-          },
-          {
-            layer: 3,
-            number: 25,
-          },
-          {
-            layer: 4,
-            number: 25,
-          },
-          {
-            layer: 5,
-            number: 25,
-          },
-        ],
-      }],
+      caselists: [
+        {
+          id: 1,
+          avatar: "hah",
+          title: "fdsf",
+          description: "fds",
+          layers: [
+            {
+              layer: 1,
+              number: 22
+            },
+            {
+              layer: 2,
+              number: 25
+            },
+            {
+              layer: 3,
+              number: 25
+            },
+            {
+              layer: 4,
+              number: 25
+            },
+            {
+              layer: 5,
+              number: 25
+            }
+          ]
+        }
+      ]
     };
   }
-  componentDidMount() {
-  }
-  handleAddCase=() => {
+  componentDidMount() {}
+  handleAddCase = () => {
     this.setState({ visible: true });
-  }
-  handleCancel=() => {
+  };
+  handleCancel = () => {
     this.setState({ visible: false });
-  }
-  layerItem=(item) => {
+  };
+  layerItem = item => {
     return (
       <List.Item key={item.layer}>
-        <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: 10, marginBottom: 10, flex: 1 }}>
-          <span>{item.layer}</span>
-          <span>{item.number}</span>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: 10,
+            marginBottom: 10,
+            flex: 1
+          }}
+        >
+          <span>第{item.layer}层</span>
+          <span>共{item.number}本</span>
         </div>
       </List.Item>
     );
-  }
-  layerList=(data) => {
+  };
+  layerList = data => {
     return (
       <List
         rowKey="id"
@@ -64,32 +73,41 @@ export default class BookCase extends PureComponent {
         renderItem={item => this.layerItem(item)}
       />
     );
-  }
-  caseItem=(item) => {
+  };
+  caseItem = item => {
     return (
       <List.Item key={item.id}>
-        <Card title="书柜" hoverable className={styles.card} actions={[<a>编辑</a>, <a>删除</a>]} >
+        <Card
+          title="书柜"
+          hoverable
+          className={styles.card}
+          actions={[<a>编辑</a>, <a>删除</a>]}
+        >
           {this.layerList(item.layers)}
         </Card>
       </List.Item>
     );
-  }
-  caseAdd=() => {
+  };
+  caseAdd = () => {
     return (
-      <List.Item >
-        <Button onClick={this.handleAddCase} type="dashed" className={styles.newButton}>
+      <List.Item>
+        <Button
+          onClick={this.handleAddCase}
+          type="dashed"
+          className={styles.newButton}
+        >
           <Icon type="plus" /> 新增书柜
         </Button>
       </List.Item>
     );
-  }
+  };
   render() {
     return (
       <div className={styles.cardList}>
         <List
           rowKey="id"
           grid={{ gutter: 24, lg: 4, md: 2, sm: 1, xs: 1 }}
-          dataSource={['', ...this.state.caselists]}
+          dataSource={["", ...this.state.caselists]}
           renderItem={item => (item ? this.caseItem(item) : this.caseAdd())}
         />
         <Modal
